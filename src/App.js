06679d8c1421631a1c05447ce5c05126c0d0c4c8
niranjana <niranju19@gmail.com>
2022-09-23 20:19:01 +0530

    change of input type

--- src/App.js
+++ src/App.js
@@ -1,52 +1,103 @@
-
+import {useState} from "react";
 import './App.css';
 
 function App() {
+  const initialValues = {
+    username: " ",
+    email: " ",
+    mobile: " ",
+    country: " ",
+    city: " ",
+    state: " ",
+    message: " ",
+  }
+  const [formValues, setFormValues] = useState(initialValues);
+
+
+
+
+
+
+
+
+
+
   return (
     <div className="container">
       <form>
         <h1>Registration Form</h1>
         <div className="ui divider"></div>
-        <div className="ui form"></div>
-        <div className="field">
-          <label>Username</label>
-          <input type="text" name="username" placeholder="Username" />
-        </div>
-        <div className="field">
-          <label>Email</label>
-          <input type="email" name="email" placeholder="Email" />
+        <div className="ui form">
+          <div className="field">
+            <label>Username</label>
+            <input type="text" 
+            name="username" 
+            placeholder="Username" 
+            value = {formValues.username} />
+            
+          </div>
+          <div className="field">
+            <label>Email</label>
+            <input 
+            type="email" 
+            name="email" 
+            placeholder="Email" 
+            value = {formValues.email} />
+
+          </div>
+          <div className="field">
+            <label>Mobile</label>
+            <input 
+            type="text" 
+            name="mobile" 
+            placeholder="Mobile" 
+            value = {formValues.mobile} />
+
+          </div>
+          <div className="field">
+            <label>Country</label>
+            <input 
+            type="text" 
+            name="country" 
+            placeholder="Country" 
+            value = {formValues.country} />
+
+          </div>
+          <div className="field">
+            <label>City</label>
+            <input 
+            type="text" 
+            name="city" 
+            placeholder="City" 
+            value = {formValues.city} />
+
+          </div>
+          <div className="field">
+            <label>State</label>
+            <input 
+            type="text" 
+            name="state" 
+            placeholder="State" 
+            value = {formValues.state} />
+
+          </div>
+          <div className="field">
+            <label>Message</label>
+            <input 
+            type="text" 
+            name="message" 
+            placeholder="Message" 
+            value = {formValues.message} />
+
+          </div>
+          <button 
+          type="button" 
+          class="btn btn-primary">Submit</button>
+
+
+
 
         </div>
-        <div className="field">
-          <label>Mobile</label>
-          <input type="mobile" name="mobile" placeholder="Mobile" />
-
-        </div>
-        <div className = "field">
-          <label>Country</label>
-          <input type = "country" name = "country" placeholder = "Country" />
-
-        </div>
-        <div className = "field">
-          <label>City</label>
-          <input type = "city" name = "city" placeholder = "City" />
-
-        </div>
-        <div className = "field">
-          <label>State</label>
-          <input type = "state" name = "state" placeholder = "State" />
-
-        </div>
-        <div className = "field">
-          <label>Message</label>
-          <input type = "message" name = "message" placeholder = "Message" />
-
-</div>
-<button type = "button" class = "btn btn-primary">Submit</button>
-
-
-
-
 
 
 
