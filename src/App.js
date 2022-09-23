9ee82e1929c7a4f9cb88fc4d7c12d6b461bb83ed
niranjana <niranju19@gmail.com>
2022-09-23 20:04:39 +0530

    adding field and button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <form>
+        <h1>Registration Form</h1>
+        <div className="ui divider"></div>
+        <div className="ui form"></div>
+        <div className="field">
+          <label>Username</label>
+          <input type="text" name="username" placeholder="Username" />
+        </div>
+        <div className="field">
+          <label>Email</label>
+          <input type="email" name="email" placeholder="Email" />
+
+        </div>
+        <div className="field">
+          <label>Mobile</label>
+          <input type="mobile" name="mobile" placeholder="Mobile" />
+
+        </div>
+        <div className = "field">
+          <label>Country</label>
+          <input type = "country" name = "country" placeholder = "Country" />
+
+        </div>
+        <div className = "field">
+          <label>City</label>
+          <input type = "city" name = "city" placeholder = "City" />
+
+        </div>
+        <div className = "field">
+          <label>State</label>
+          <input type = "state" name = "state" placeholder = "State" />
+
+        </div>
+        <div className = "field">
+          <label>Message</label>
+          <input type = "message" name = "message" placeholder = "Message" />
+
+</div>
+<button type = "button" class = "btn btn-primary">Submit</button>
+
+
+
+
+
+
+
+      </form>
     </div>
+
+
+
   );
 }
 
