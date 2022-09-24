a4fcada0eef8eb9539ff69840b6fea8c6f02347e
niranjana <niranju19@gmail.com>
2022-09-24 21:15:42 +0530

    form validation

--- src/App.js
+++ src/App.js
@@ -1,162 +1,166 @@
-import {useState, useEffect} from "react";
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
   const initialValues = {
-    username:" ",
-    email:" ",
-    mobile:" ",
-    country:" ",
-    city:" ",
-    state:" ",
-    message:" ",
+    username: " ",
+    email: " ",
+    mobile: " ",
+    country: " ",
+    city: " ",
+    state: " ",
+    message: " ",
   };
-  
+
   const [formValues, setFormValues] = useState(initialValues);
   const [formErrors, setFormErrors] = useState({});
-  const [isSubmit, setIsSubmit]= useState(false);
+  const [isSubmit, setIsSubmit] = useState(false);
 
 
-const handleChange = (e) => {
-  console.log(e.target);
-  const {name, value} = e.target;
-  setFormValues ({...formValues, [name]:value
-  });
- 
+  const handleChange = (e) => {
 
-}
-
-const handleSubmit = (e) => {
-  e.preventDefault();
-  setFormErrors(validate(formValues));
-  setIsSubmit(true);
-};
-
-useEffect(() => {
-  console.log(formErrors);
-  if(Object.keys(formErrors).length === 0 && isSubmit){
-    console.log(formValues);
-  }
-},[formErrors]);
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
 
 
 
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
+  };
+
+  useEffect(() => {
+    console.log(formErrors);
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      console.log(formValues);
+    }
+  }, [formErrors]);
+
+  const validate = (values) => {
+    const errors = {}
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    if (!values.username) {
+      errors.username = "Username is required!";
+
+    }
+
+    if (!values.email) {
+      errors.email = "Email is required!";
+
+    } else if (!regex.test(values.email)) {
+      errors.email = "This is not a valid email format";
+    }
 
 
 
-
-
-const validate = (values) => {
-  const errors = {}
-  const regex =/^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i; 
-  if (!values.username) {
-    errors.username = "Username is required!";
-
-  }
-
-  if(!values.email){
-errors.username = "Email is required!";
-  }
-
- 
-
-  return errors;
-}
+    return errors;
+  };
 
 
 
 
   return (
     <div className="container">
-      <pre>{JSON.stringify(formValues,undefined, 2)}</pre>
-      <form onSubmit = {handleSubmit} >
+      {Object.keys(formErrors).length === 0 && isSubmit ? (
+        <div className="ui message success"> Signed in successfully </div>
+      ) : (
+        <pre>{JSON.stringify(formValues, undefined, 2)}</pre>
+      )}
+
+
+
+      <form onSubmit={handleSubmit} >
         <h1>Registration Form</h1>
         <div className="ui divider"></div>
         <div className="ui form">
           <div className="field">
             <label>Username</label>
-            <input 
-            type="text" 
-            name="username" 
-            placeholder="Username" 
-            value = {formValues.username} 
-            onChange = {handleChange}
-            />
-            
-          </div>
-          <div className="field">
-            <label>Email</label>
-            <input 
-            type="email" 
-            name="email" 
-            placeholder="Email" 
-            value = {formValues.email} 
-            onChange = {handleChange}
+            <input
+              type="text"
+              name="username"
+              placeholder="Username"
+              value={formValues.username}
+              onChange={handleChange}
             />
 
           </div>
+          <p>{formErrors.username}</p>
+          <div className="field">
+            <label>Email</label>
+            <input
+              type="text"
+              name="email"
+              placeholder="Email"
+              value={formValues.email}
+              onChange={handleChange}
+            />
+
+          </div>
+          <p>{formErrors.email}</p>
           <div className="field">
             <label>Mobile</label>
-            <input 
-            type="text" 
-            name="mobile" 
-            placeholder="Mobile" 
-            value = {formValues.mobile} 
-            onChange = {handleChange}
+            <input
+              type="text"
+              name="mobile"
+              placeholder="Mobile"
+              value={formValues.mobile}
+              onChange={handleChange}
             />
 
 
           </div>
           <div className="field">
             <label>Country</label>
-            <input 
-            type="text" 
-            name="country" 
-            placeholder="Country" 
-            value = {formValues.country} 
-            onChange = {handleChange}
+            <input
+              type="text"
+              name="country"
+              placeholder="Country"
+              value={formValues.country}
+              onChange={handleChange}
             />
 
 
           </div>
           <div className="field">
             <label>City</label>
-            <input 
-            type="text" 
-            name="city" 
-            placeholder="City" 
-            value = {formValues.city} 
-            onChange = {handleChange}
+            <input
+              type="text"
+              name="city"
+              placeholder="City"
+              value={formValues.city}
+              onChange={handleChange}
             />
 
           </div>
           <div className="field">
             <label>State</label>
-            <input 
-            type="text" 
-            name="state" 
-            placeholder="State" 
-            value = {formValues.state} 
-            onChange = {handleChange}
+            <input
+              type="text"
+              name="state"
+              placeholder="State"
+              value={formValues.state}
+              onChange={handleChange}
             />
 
           </div>
           <div className="field">
             <label>Message</label>
-            <input 
-            type="text" 
-            name="message" 
-            placeholder="Message" 
-            value = {formValues.message} 
-            onChange = {handleChange}
+            <input
+              type="text"
+              name="message"
+              placeholder="Message"
+              value={formValues.message}
+              onChange={handleChange}
             />
 
           </div>
-          <p>{formErrors.password}</p>
-          <button 
-          type="button" 
-          class="btn btn-primary">Submit</button>
+
+          <button
+            type="button"
+            className="fluid ui button blue">Submit</button>
 
 
 
@@ -174,3 +178,5 @@ errors.username = "Email is required!";
 }
 
 export default App;
+
+
